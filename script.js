be06c750c3b2c17132c855342c3c2fb147d2538f
Acidias <dmisi98@gmail.com>
2021-09-14 01:38:48 +0100

    Section Projects Update

--- script.js
+++ script.js
@@ -11,5 +11,5 @@ $(document).ready(function() {
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
-    });
+    }); 
 });
